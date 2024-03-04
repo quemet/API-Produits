c733d1c423dcc4e61861d760e27a01b54fafaf55
Quentin Métroz <quentinmet7@gmail.com>
2024-03-04 13:08:38 +0100

    Ajout du code de la step02

--- src/app.mjs
+++ src/app.mjs
@@ -1,12 +1,20 @@
 import express from "express";
 
+import { productsRouter } from "./routes/products.mjs";
+
 const app = express();
 
 const port = 3000;
 
 app.get("/", (req, res) => {
-   res.send("Hello, World!");
+   res.send("API REST of self service machine !");
 });
+
+app.get("/api/", (req, res) => {
+   res.redirect(`http://localhost:${port}`);
+});
+
+app.use("/api/products", productsRouter);
 
 app.listen(port, () => {
    console.log(`Example app listening on port http://localhost:${port}`);
---
+++ src/routes/products.mjs
@@ -0,0 +1,14 @@
+import express from "express";
+
+import { products } from "../db/mock-products.mjs";
+
+import { success } from "./helper.mjs";
+
+const productsRouter = express();
+
+productsRouter.get("/", (req, res) => {
+   const message = "La liste des produits a bien été récupérée";
+   res.json(success(message, products));
+});
+
+export { productsRouter };
